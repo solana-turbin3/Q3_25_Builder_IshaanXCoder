5346b6630628cee5a3ac6fe76e9e31ba56f72b4f
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2025-07-03 19:51:38 +0530

    push rug day

---
+++ 1-3-rug-day-nft/nft_mint.ts
@@ -0,0 +1,33 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
+import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+
+import wallet from "../1-1-solana-starter/ts/turbin3-wallet.json"
+import base58 from "bs58";
+
+const RPC_ENDPOINT = "https://api.devnet.solana.com";
+const umi = createUmi(RPC_ENDPOINT);
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const myKeypairSigner = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(myKeypairSigner));
+umi.use(mplTokenMetadata())
+
+const mint = generateSigner(umi);
+
+(async () => {
+    let tx = createNft(umi, {
+        mint: mint,
+        name: "RugMe",
+        symbol: "RGM",
+        uri: "https://gateway.irys.xyz/BdLuytrBuKfcFNufwNvETVEPhoUJ3poxWpkAmgHkksfF",
+        sellerFeeBasisPoints: percentAmount(100, 2),
+        isCollection: false,
+    });
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+    
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+
+    console.log("Mint Address: ", mint.publicKey);
+})();
